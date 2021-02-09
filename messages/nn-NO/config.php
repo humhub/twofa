0b39ca34ad0d09a3707373ff26a96448dbee4c99
HumHub Translations <info@humhub.com>
2021-02-09 15:43:18 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/nn-NO/config.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    '<strong>Request</strong> new code' => '',
+    '<strong>Two-Factor Authentication</strong> module configuration' => '',
+    '<strong>Two-Factor Authentication</strong> settings' => '',
+    'Account:' => '',
+    'By default, the method "{defaultDriverName}" is used.' => '',
+    'Can\'t scan the code?' => '',
+    'Do you really want to request a new code?' => '',
+    'Enabled methods' => '',
+    'Install an application that implements a time-based one-time password (TOTP) algorithm, such as <a{docLinkAttrs}>Google Authenticator</a>, and use it to scan the QR code shown below.' => '',
+    'Length of verifying code' => '',
+    'Mandatory for the following groups' => '',
+    'Please <strong>do not forget</strong> to update the code in your authenticator app! If you do not do so, you will not be able to login.' => '',
+    'QR Code' => '',
+    'Request new code' => '',
+    'Secret:' => '',
+    'This module is disabled because no drivers are selected, however users from the enforced groups always fallback to {defaultDriverName} driver by default.' => '',
+    'Time based: Yes' => '',
+    'To connect the app manually, provide the following details to the TOTP app (e.g. Google Authenticator).' => '',
+    'Two-Factor Authentication' => '',
+    'Two-factor authentication (2FA) provides an additional level of security for your account. Once enabled, you will be prompted to enter a code in addition to entering your username and password.' => '',
+];
