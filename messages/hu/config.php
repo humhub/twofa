1b56eefa1aa3ea5bb5905f4c26127db1478aeda7
HumHub Translations <hello@humhub.com>
2023-01-20 19:01:02 +0000

    Fixed messages

--- messages/hu/config.php
+++ messages/hu/config.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 return array (
   '<strong>Request</strong> new code' => 'Új kód <strong>igénylése</strong>',
   '<strong>Two-Factor Authentication</strong> module configuration' => '<strong>Kétlépcsős azonosítás</strong> modul konfigurálás',
@@ -26,32 +25,3 @@ return array (
   'Two-Factor Authentication' => 'Kétlépcsős azonosítás',
   'Two-factor authentication (2FA) provides an additional level of security for your account. Once enabled, you will be prompted to enter a code in addition to entering your username and password.' => 'A kétlépcsős azonosítás (2FA) nagyobb biztonságot nyújt a fiókod számára. Az engedélyezés után a rendszer kéri a kód megadását a felhasználónév és jelszó azonosítása után.',
 );
-=======
-
-return [
-    '<strong>Request</strong> new code' => 'Új kód <strong>igénylése</strong>',
-    '<strong>Two-Factor Authentication</strong> module configuration' => '<strong>Kétlépcsős azonosítás</strong> modul konfigurálás',
-    '<strong>Two-Factor Authentication</strong> settings' => '<strong>Kétlépcsős azonosítás</strong> beállítások',
-    'Account:' => 'Fiók:',
-    'Can\'t scan the code?' => 'Nem tudod beolvasni a kódot?',
-    'Do you really want to request a new code?' => 'Biztosan szeretnél új kódot kérni?',
-    'Enabled methods' => 'Engedélyezett módszerek',
-    'Google Authenticator' => 'Google Authenticator',
-    'Install an application that implements a time-based one-time password (TOTP) algorithm, such as {googleAuthenticatorLink}, and use it to scan the QR code shown below.' => 'Telepíts egy olyan alkalmazást, amely "időalapú egyszeri jelszó" (TOTP) algoritmust használ, ilyen például a {googleAuthenticatorLink}, ezzel olvasd be az alább látható QR-kódot.',
-    'Length of verifying code' => 'Visszaigazoló kód hossza',
-    'Mandatory for the following groups' => 'A következő csoportok számára kötelező',
-    'Please <strong>do not forget</strong> to update the code in your authenticator app! If you do not do so, you will not be able to login.' => 'Kérjük, <strong>ne felejtsd el</strong> frissíteni a kódot a hitelesítő alkalmazásban! Ha nem teszel így, akkor nem fogsz tudni bejelentkezni.',
-    'Request new code' => 'Új kód kérése',
-    'Secret:' => 'Titkos:',
-    'This module is disabled because no drivers are selected, however users from the enforced groups always fallback to {defaultDriverName} driver by default.' => 'Ez a modul le van tiltva, mert nincsenek kiválasztva vezetők, azonban a kényszerített csoportok felhasználói alapértelmezés szerint mindig a(z) {defaultDriverName} vezetőre állnak vissza.',
-    'Time based: Yes' => 'Időalapú: Igen',
-    'To connect the app manually, provide the following details to the TOTP app (e.g. Google Authenticator).' => 'Az alkalmazás manuális csatlakoztatásához add meg a következő részleteket a TOTP alkalmazáshoz (pl.: Google Authenticator).',
-    'Two-Factor Authentication' => 'Kétlépcsős azonosítás',
-    'Two-factor authentication (2FA) provides an additional level of security for your account. Once enabled, you will be prompted to enter a code in addition to entering your username and password.' => 'A kétlépcsős azonosítás (2FA) nagyobb biztonságot nyújt a fiókod számára. Az engedélyezés után a rendszer kéri a kód megadását a felhasználónév és jelszó azonosítása után.',
-    'Default method for the mandatory groups' => '',
-    'Leave empty to disable this feature.' => '',
-    'List of IPs or subnets to whitelist, currently yours is {0}. Use coma separator to create a list, example: "{0}, 127.0.0.1"' => '',
-    'Remember browser option amount of days' => '',
-    'Trusted networks list' => '',
-];
->>>>>>> 8930fb7fd719545ec3baa69a422c15bd57536822
